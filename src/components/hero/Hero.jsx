c6897713d1aac036f16541d0ad2665c3c079293d
rash <orashusedmund@gmail.com>
2023-01-22 12:08:28 +0100

    done with the gallery

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,8 +7,12 @@ import goldGoRight from '../../images/hero gold move forward icon.png';
 function Hero({ text1, text2, text3, text4, spanText, createHeroNav = true }) {
   return (
     <div className="hero">
-      <h1 className="text1">{text1}</h1>
-      <h3 className="text2">{text2}</h3>
+      <h1 className="text1">
+        <span>{text1}</span>
+      </h1>
+      <h3 className="text2">
+        <span>{text2}</span>
+      </h3>
 
       <h1 className="text3">{text3 !== '' ? text3 : ''}</h1>
       <h3 className="text4">
--- src/gallery/galleryPhotos/GalleryPhotos.jsx
+++ src/gallery/galleryPhotos/GalleryPhotos.jsx
@@ -1,60 +1,64 @@
 import './galleryPhotos.css';
-// import data from './galleryPhotosData.json';
-import girrafe from '../../images/girrafe.png';
+import data from './galleryPhotosData.json';
+
+const renderGalleryPhotos = () => {
+  return data.map((piece) => {
+    return piece.map((subPiece) => {
+      return (
+        <div className="galleryPhotos__rowCard">
+          <div className="rowCard__mainCol1">
+            <div className="mainCol1__subCol1">
+              <div
+                className="subOfSub1"
+                style={{ backgroundImage: `URL(${subPiece.src})` }}
+              />
+              <div
+                className="subOfSub2"
+                style={{ backgroundImage: `URL(${subPiece.src})` }}
+              />
+            </div>
+
+            <div
+              className="mainCol1__subCol2"
+              style={{ backgroundImage: `URL(${subPiece.src})` }}
+            />
+          </div>
+
+          <div className="rowCard__mainCol2">
+            <div
+              className="mainCol2__subCol1"
+              style={{ backgroundImage: `URL(${subPiece.src})` }}
+            />
+            <div
+              className="mainCol2__subCol2"
+              style={{ backgroundImage: `URL(${subPiece.src})` }}
+            />
+          </div>
+
+          <div className="rowCard__mainCol3">
+            <div
+              className="mainCol3__subCol1"
+              style={{ backgroundImage: `URL(${subPiece.src})` }}
+            />
+            <div className="mainCol3__subCol2">
+              <div
+                className="subOfSub1"
+                style={{ backgroundImage: `URL(${subPiece.src})` }}
+              />
+              <div
+                className="subOfSub2"
+                style={{ backgroundImage: `URL(${subPiece.src})` }}
+              />
+            </div>
+          </div>
+        </div>
+      );
+    });
+  });
+};
 
 function GalleryPhotos() {
-  /* const renderGalleryPhotos = () => {
-    return (
-      <>
-        <p>go</p>
-        <div>go</div>
-      </>
-    );
-  }; */
-
-  return (
-    <div className="galleryPhotos">
-      <div className="galleryPhotos__rowCard">
-        <div className="rowCard__mainCol1">
-          <div className="mainCol1__subCol1">
-            <div className="subOfSub1">
-              <img src={girrafe} alt="data" />
-            </div>
-            <div className="subOfSub2">
-              <img src={girrafe} alt="data" />
-            </div>
-          </div>
-
-          <div className="mainCol1__subCol2">
-            <img src={girrafe} alt="data" />
-          </div>
-        </div>
-
-        <div className="rowCard__mainCol2">
-          <div className="mainCol2__subCol1">
-            <img src={girrafe} alt="data" />
-          </div>
-          <div className="mainCol2__subCol2">
-            <img src={girrafe} alt="data" />
-          </div>
-        </div>
-
-        <div className="rowCard__mainCol3">
-          <div className="mainCol3__subCol1">
-            <img src={girrafe} alt="data" />
-          </div>
-          <div className="mainCol3__subCol2">
-            <div className="subOfSub1">
-              <img src={girrafe} alt="data" />
-            </div>
-            <div className="subOfSub2">
-              <img src={girrafe} alt="data" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="galleryPhotos">{renderGalleryPhotos()}</div>;
 }
 
 export default GalleryPhotos;
